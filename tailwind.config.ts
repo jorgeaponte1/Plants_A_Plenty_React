63a5775a77a078987402684ddb8c0200069aef87
Jorge Aponte <jorgealyaponte@yahoo.com>
2023-10-01 15:39:27 -0700

    Added NextUI and attached Provider to App

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
+import { nextui } from "@nextui-org/react"
 import type { Config } from "tailwindcss"
 
 const config: Config = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     container: {
@@ -50,6 +52,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), nextui()],
 }
 export default config
